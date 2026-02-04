31d93640dbf888568271bfa3cf5b27f5d1fffa1c
REHAN SV <rehansv1@gmail.com>
2026-02-04 10:14:14 +0530

    Add multiple Java programs: ArrayProcessor, Calculator, MenuCalculator, MultiTable, number_Table, and piramid_number

---
+++ _Java/piramid_number.java
@@ -0,0 +1,16 @@
+package _Java;
+public class piramid_number {
+    public static void main(String[] args){
+int n = 3;
+int num = 1;
+
+for(int i=1;i<=n;i++){
+    for(int s=n-i; s>0; s--) System.out.print(" ");
+    for(int j=1;j<=i;j++){
+        System.out.print(num + " ");
+        num++;
+    }
+    System.out.println();
+    }
+    }
+}   
