ab8b5f73b2ebec48fafd36bd23014712ac3bef7f
REHAN SV <rehansv1@gmail.com>
2026-04-08 14:45:59 +0530

    new

--- implication.py
+++
@@ -1,8 +0,0 @@
-def implication(p,q):  
-    return not p or q
-
-value=[True,False]
-print("p \t q \t p->q")
-for p in value:
-    for q in value:
-        print(f"{p} \t {q} \t {implication(p,q)}")
